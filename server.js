c9f32d0a935d2af2d82bcbdcf159e8ddc265b24e
Brian Hines <brian@projectweekend.net>
2015-01-25 21:34:25 -0600

    Better get this in there too

--- server.js
+++ server.js
@@ -5,13 +5,33 @@ var transitStops = require( "./api/transit-stops/controllers" );
 
 mongoose.connect( process.env.MONGO_URL || process.env.DB_1_PORT.replace( "tcp", "mongodb" ) + "/test" );
 
+if ( typeof process.env.API_KEY === "undefined" ) {
+    console.log( "API_KEY environment variable not defined." );
+    process.exit( 1 );
+}
+
 
 var server = restify.createServer( {
     name: 'Transit-Stop-API',
 } );
 
 
-server.get( "/", transitStops.test );
+// middleware
+server.use( restify.queryParser() );
+server.use( function ( req, res, next ) {
+    if ( req.header( "API_KEY" ) !== process.env.API_KEY ) {
+        return res.send( 403, { message: "Invalid API key" } );
+    }
+    next();
+} );
 
 
+// routes
+server.get( "/:transitSystem", transitStops.getRoutes );
+server.get( "/:transitSystem/train/:routeId", transitStops.getTrainStops );
+server.get( "/:transitSystem/bus/:routeId", transitStops.getBusStops );
+server.get( "/:transitSystem/bus/:routeId/:routeDirection", transitStops.getBusStopsForDirection );
+
+
+// run
 server.listen( process.env.PORT || 3000 );
