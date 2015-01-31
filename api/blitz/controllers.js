3c5ae8936533759b0aca33d16696e676bb69ada4
Brian Hines <brian@projectweekend.net>
2015-01-31 13:11:29 -0600

    Seriously

--- api/blitz/controllers.js
+++ api/blitz/controllers.js
@@ -1,8 +1,7 @@
 exports.get = function ( req, res ) {
 
-    res.writeHead( 200, {
-        "Content-Type": "text/plain"
-    } );
+    res.setHeader( "Content-Type", "text/plain" );
+    res.writeHead( 200 );
     res.body( "42" );
     res.end();
 
