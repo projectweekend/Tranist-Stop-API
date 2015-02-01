522616454eb4567da3c4829d26a93ccfc2d9a358
Brian Hines <brian@projectweekend.net>
2015-02-01 00:50:47 -0600

    Put this back for now

--- api/transit-stop/handlers.js
+++ api/transit-stop/handlers.js
@@ -1,63 +1,28 @@
 var TransitStop = require( "./models" ).TransitStop;
 
 
-var BaseHandler = function () {
+exports.getTrain = function ( req, res ) {
 
-    var _this = this;
-
-    return function ( req, res ) {
-
-        _this.filter = {
-            system: req.params.transitSystem.toLowerCase(),
-            route_id: req.params.routeId.toLowerCase()
-        };
-
-        if ( typeof req.query.name !== "undefined" ) {
-            _this.filter.name = new RegExp( req.query.name, "i" );
-        }
-
-        return _this.getData( _this.filter, _this.sendResponse( res ) );
-
+    var filter = {
+        system: req.params.transitSystem.toLowerCase(),
+        route_id: req.params.routeId.toLowerCase()
     };
 
-};
+    if ( typeof req.query.name !== "undefined" ) {
+        filter.name = new RegExp( req.query.name, "i" );
+    }
 
-BaseHandler.prototype.getData = function ( filter, done ) {
-
-};
-
-BaseHandler.prototype.sendResponse = function( res ) {
-
-    return function ( err, results ) {
+    TransitStop.forTrain( filter, function( err, results ) {
 
         if ( err ) {
-            return res.send( 500, { message: "Database error" } );
+            return this.res.send( 500, { message: "Database error" } );
         }
 
-        return res.send( 200, results );
+        return this.res.send( 200, results );
 
-    };
-
-};
-
-
-var TrainHandler = function () {
-
-    BaseHandler.call( this );
+    } );
 
 };
-
-TrainHandler.prototype = Object.create( BaseHandler.prototype );
-TrainHandler.prototype.constructor = TrainHandler;
-TrainHandler.prototype.getData = function ( filter, done ) {
-
-    TransitStop.forTrain( filter, done );
-
-};
-
-
-
-exports.getTrain = new TrainHandler();
 
 
 exports.getBus = function ( req, res ) {
